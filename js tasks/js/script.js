97106e5f1e3ab4d716823930e3d112ce11da19fd
AhmedFelic <ahmedfelic1@gmail.com>
2022-02-17 17:13:30 +0100

    cross/uncross delete all done

--- js tasks/js/script.js
+++ js tasks/js/script.js
@@ -1,4 +1,21 @@
-function funkcija1(){
-    var inputVal = document.getElementById("wishh").value;
-    alert(inputVal);
+
+function funkcija1() {
+
+    let inputVal = document.getElementById("wishh").value;
+    if (inputVal.length > 11150) {
+        alert("Input is too big, try again!");
+    } else {
+        document.getElementById("t1").innerHTML += inputVal;
+    }
+
+}
+function f2() {
+
+    document.getElementsByTagName("p")[1].childNodes[0].nodeValue = "";
+}
+var flag = true;
+function f3() {
+    let el = document.getElementById("t1");
+    el.style.textDecoration = flag ? "line-through" : "";
+    flag = !flag;
 }
