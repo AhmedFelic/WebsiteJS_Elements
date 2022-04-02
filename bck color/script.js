cd31bbe81ce7c1e8c1cd6c4b75d7200652894643
AhmedFelic <ahmedfelic1@gmail.com>
2022-04-02 22:05:28 +0200

    done

--- bck color/script.js
+++ bck color/script.js
@@ -2,9 +2,11 @@
 function newFunction() {
     var a = document.getElementById('clr').value;
     document.getElementsByTagName('div')[0].style.background = a;
-
+    if (a == "black") {
+        document.getElementsByTagName('div')[0].style.color = "white";
+    }
 }
 function newFunction1() {
     var b = document.getElementsByTagName('div')[0].style.background = "white";
-
+    document.getElementsByTagName('div')[0].style.color = "black";
 }
